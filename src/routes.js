cfc933f6ca5e6a6ec17d74f2e7b5dc4e2a8d73e3
jonataspinto <jonatastdb_jp@hotmail.com>
2020-02-03 17:57:08 -0300

    feat: crud completo

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,26 @@
 const { Router } = require('express');
 
-const { validateData } = require('./middlewares/data-validation.middleware')
+const { validateData } = require('./middlewares/data-validation.middleware');
 
 const routes = Router()
 
 const projects = [
     {
-        id: 1,
+        id: "1",
         title: "Novo Projeto",
+        tasks: [
+            "implementar redux"
+        ]
+    },
+    {
+        id: "3",
+        title: "youpluv",
         tasks: []
     }
 ]
 
 routes.post('/projects', validateData, (req, res)=>{
-
     projects.push(req.body)
-
     return res.json(projects)
 })
 
@@ -23,8 +28,27 @@ routes.get('/projects', (req, res)=>{
     return res.json(projects)
 })
 
-routes.put('/projects/:id', ()=>{
+routes.put('/projects/:id', (req, res)=>{
+    const { id } = req.params
+    const { title } = req.body
+    const project =  projects.find(res=> res.id === id)
+    project.title = title
+    return res.json(projects)
+})
 
+routes.delete('/projects/:id', (req, res )=>{
+    const { id } = req.params
+    const index = projects.findIndex(res=> res.id == id)
+    projects.splice(index, 1)
+    return res.json(projects)
+})
+
+routes.post('/projects/:id/tasks',(req, res)=>{
+    const { id } = req.params
+    const { newTask } = req.body
+    const project = projects.find(res=> res.id === id)
+    project.tasks.push(newTask)
+    return res.send(projects)
 })
 
 
